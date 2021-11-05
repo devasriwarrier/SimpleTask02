c6f15e21bf3c108a534a749897c8f42ed8364043
Devasri Warrier <devasri.warrier@mavs.uta.edu>
2021-11-05 15:13:34 -0500

    adding notes deleting notes

---
+++ app/src/main/java/com/example/simpletask02/AddNoteActivity.java
@@ -0,0 +1,47 @@
+package com.example.simpletask02;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import io.realm.Realm;
+
+public class AddNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+
+        EditText titleInput = findViewById(R.id.titleinput);
+        EditText descriptionInput = findViewById(R.id.descriptioninput);
+        Button saveBtn = findViewById(R.id.save_btn);
+
+
+        Realm.init(getApplicationContext());
+        Realm realm = Realm.getDefaultInstance();
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = titleInput.getText().toString();
+                String description = descriptionInput.getText().toString();
+                long createdTime = System.currentTimeMillis();
+
+                realm.beginTransaction();
+                Note note = realm.createObject(Note.class);
+                note.setTitle(title);
+                note.setDescription(description);
+                note.setCreatedTime(createdTime);
+                realm.commitTransaction();
+                Toast.makeText(getApplicationContext(),"Note saved!",Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+
+
+    }
+}
